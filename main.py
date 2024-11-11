757c8810a7be5bef687669b2d77675cde72e1b2a
TahaGabu <tahagabu5354@gmail.com>
2024-11-11 17:29:58 +0530

    update Env Keys.

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ This will install the packages from the requirements.txt for this project.
 
 app = Flask(__name__)
 # app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-app.config["SECRET KEY"] = os.environ.get("FLASK_KEY")
+app.config["SECRET_KEY"] = os.environ.get("FLASK_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -49,7 +49,7 @@ class Base(DeclarativeBase):
 
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URI", 'sqlite:///blog.db')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URI", "sqlite:///blog.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -312,4 +312,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
